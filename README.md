efcdc9de6e08f1d7760e0e8cfc3bca8a59cf1aa6
Nadia Idris <nadia@fasterlap.com>
2022-11-27 21:01:30 -0800

    WIP

--- README.md
+++ README.md
@@ -1,7 +1,14 @@
-# About Version History
+# Version history
 
-This is a TypeScript project illustrating how to implement undo/redo functionality in a version
-  history.
+Implement an editor class which allows to edit a file by adding and deleting words. It will also have methods called undo and redo.
+
+Editor actions:
+- `add(word: string): void` - appends a word to the end of the current text.
+- `delete()` - deletes the last word in the editor.
+- `edit(newWord: string): void` - replaces the last word in the editor with the given word.
+- `undo(): void` - undoes the last action (actions are: adding, deleting, editing).
+- `redo(): void` - redoes the last undone action. We can redo an action only if we have undone it previously.
+- `printString(): string` - prints the current text in the editor.
 
 ![](/readme-assets/version-history.png)
 
@@ -11,6 +18,42 @@ This is a TypeScript project illustrating how to implement undo/redo functionali
 
 ## How to run tests
 - `npx jest --watchAll` to run the tests
+
+# Implementation
+
+## Data modeling
+
+We know we want to be able to add, delete and edit words. Each word is a string. We need to be able to add/remove/edit words easily. We want to use array of strings to store the words. 
+
+In order to implement undo, we must be able to revert to a previous state of the editor. We can store each state of the editor in another array. We can use a stack to store the states. Let's call that array `states `
+
+`undo` - To implement undo we need to create a new property in our class to track the current state to display. Let's call that indexOfStateToDisplay.
+
+Let's update our interface to include the indexOfStateToDisplay property. It is a type number.
+
+```typescript
+interface EditorInterface {
+  editorState: EditorState;
+  history: EditorState[];
+  indexOfStateToDisplay: number;
+  add: (word: string) => void;
+  deleteLastWord: () => void;
+  replaceLastWord: (newWord: string) => void;
+  undo: () => void; 
+  redo: () => void;
+}
+```
+
+If I have undo() in the middle of the history and I add a new word -> update the editorState with the new word, add to the end of the editorHistory the new state, and set the indexOfStateToDisplay to the end of the history.
+
+```typescript
+add(word: string) {
+  this.editorState.words.push(word);
+  this.history.push(this.editorState);
+  this.indexOfStateToDisplay = this.history.length - 1;
+}
+``` 
+
 
 ## Instructions
 
---
+++ src/main1.ts
@@ -0,0 +1,194 @@
+type EditorState = string[];
+
+/*
+ Data examples when writing methods:
+ 
+  editorState = ["first", "second", "third" ]
+  history = [
+    ["first"],
+    ["first", "second"], <- editorState. Delete "second".
+    ["first"]
+
+
+
+    ["first", "second", "third"]
+ */
+
+interface EditorInterface {
+  editorState: EditorState;
+  history: EditorState[];
+  indexToDisplay: number;
+  add: (word: string) => void; 
+  delete: () => void;
+  edit: (newWord: string) => void;
+  undo: () => void;
+  redo: () => void;
+}
+
+class Editor implements EditorInterface {
+  editorState: EditorState = [];
+  history: EditorState[] = [];
+  indexToDisplay: number = 0;
+
+  // /** Take the word and add it to the editorState array. Then add the editorState array to history array. */
+  // add = (word: string) => {
+  //   // Make a copy of editorState and modify the copy.
+  //   const copyOfState = [...this.editorState];
+  //   copyOfState.push(word);
+  //   // If history is empty, add the word to the history array.
+  //   if (this.history.length === 0) {
+  //     // Update the editorState and history with the new editorState.
+  //     this.editorState = copyOfState;
+  //     this.history.push(copyOfState);
+  //     this.indexToDisplay = 0;
+  //     return;
+  //   }
+  //   const lastIndexOfHistory = this.history.length - 1;
+  //   // If indexToDisplay is in the middle or start of the array.
+  //   if (this.indexToDisplay !== lastIndexOfHistory) {
+  //     // Chop off the history array from the indexOfStateToDisplay till the end.
+  //     this.history.splice(this.indexToDisplay + 1);
+  //   }
+
+  //   // Update the editorState and history with the new editorState.
+  //   this.editorState = copyOfState;
+  //   this.history.push(copyOfState);
+  //   // Every time when we add a word, we need to update the indexOfStateToDisplay.
+  //   this.history.length === 1
+  //     ? (this.indexToDisplay = 0)
+  //     : this.indexToDisplay++;
+  // };
+
+  // Add method without making a copy of editorState.
+  /** Take the word and add it to the editorState array. Then add the editorState array to history array. */
+  add = (word: string) => {
+    this.editorState.push(word);
+
+    if (this.history.length === 0) {
+      this.history.push(this.editorState);
+      this.indexToDisplay = 0;
+      return;
+    }
+
+    const lastIndexOfHistory = this.history.length - 1;
+    if (this.indexToDisplay !== lastIndexOfHistory) {
+      this.history.splice(this.indexToDisplay + 1);
+    }
+
+    this.history.push(this.editorState);
+    // Every time when we add a word, we need to update the indexOfStateToDisplay.
+    this.history.length === 1
+      ? (this.indexToDisplay = 0)
+      : this.indexToDisplay++;
+  };
+
+  /** Remove the last word from the state array. Then add the new state to history array. */
+  delete = () => {
+    // Make a copy of editorState and modify the copy.
+    const copyOfState = [...this.editorState];
+    copyOfState.pop();
+    const lastIndexOfHistory = this.history.length - 1;
+    // indexToDisplay is in the middle or start of the array.
+    if (this.indexToDisplay !== lastIndexOfHistory) {
+      this.history.splice(this.indexToDisplay + 1);
+    }
+    // Update the editorState and history with the new editorState.
+    this.editorState = copyOfState;
+    this.history.push(copyOfState);
+    // Every time when we delete a word, we need to update the indexOfStateToDisplay by once,
+    // since we have added a new state with missing word.
+    this.indexToDisplay++;
+  };
+
+  /** Remove the last word from the state array. Add the new word to the end of the state array.
+   *  Aft that add the new state to history array. */
+  edit = (newWord: string) => {
+    // Make a copy of editorState and modify the copy.
+    const copyOfState = [...this.editorState];
+    copyOfState.pop();
+    copyOfState.push(newWord);
+    const lastIndexOfHistory = this.history.length - 1;
+    if (this.indexToDisplay !== lastIndexOfHistory) {
+      this.history.splice(this.indexToDisplay + 1);
+    }
+    // Update the editorState and history with the new editorState.
+    this.editorState = copyOfState;
+    this.history.push(copyOfState);
+  };
+
+  /* 
+  When we are always modifying the last editorState in the history array.
+  Adding:
+    history = [  <-Add "first"
+    ["first"],   <-Add "second"
+    ["first", "second"],  <- Add "third"  
+    ["first", "second", "third"]
+    ]
+
+  Deleting:
+    history = [  <-Add "first"
+    ["first"],   <-Add "second"
+    ["first", "second"],  <- Add "third"  
+    ["first", "second", "third"]  <- Delete the last word
+    ["first", "second"],
+    ]
+
+    Editing:
+    history = [  <-Add "first"
+    ["first"],   <-Add "second"
+    ["first", "second"],    <-Add "third"
+    ["first", "second", "third"] <- Edit the last word
+    ["first", "second", "fourth"]
+    ]
+--------------------------------
+  When we are modifying the editorState which is in the middle of the array.
+    Adding:
+    history = [  <-Add "first"
+    ["first"],   <-Add "second"
+    ["first", "second"],  <- Add "third"   <- Add "fourth" 
+    ["first", "second", "fourth"]
+
+    ["first", "second", "third"] <- undo  ----- Delete this
+    ]
+
+    Deleting:
+    history = [  <-Add "first"
+    ["first"],   <-Add "second"
+    ["first", "second"],  <- Add "third"  
+    ["first", "second", "third"]  <- Delete the last word
+    ["first", "second"], <- undo
+    ]
+
+  */
+
+  /**  Move indexOfStateToDisplay closer to the start of the array */
+  undo = () => {
+    if (this.indexToDisplay > 0) {
+      this.indexToDisplay--;
+      this.editorState = this.history[this.indexToDisplay];
+    } else if (this.indexToDisplay <= 0) {
+      this.indexToDisplay = -1; // Don't allow the index to go below 0.
+      this.editorState = [];
+    }
+  };
+
+  /** Move indexOfStateToDisplay closer to the end of the array */
+  redo = () => {
+    const lastIndexOfHistory = this.history.length - 1;
+    if (this.indexToDisplay < lastIndexOfHistory) {
+      this.indexToDisplay++;
+      this.editorState = this.history[this.indexToDisplay];
+    } else {
+      // Display the last editorState in the history array.
+      this.editorState = this.history[lastIndexOfHistory];
+    }
+  };
+
+  // TODO: delete the stuff below.
+  private _myPrivateFunction = () => {}; // arrow functions are visibile in the class when console.logging the propeties.
+  private myPrivateFunction2() { } // function definitions inside classes are not visible in the console.
+  myPublicFunction() { } // function definitions inside classes are not visible in the console. 
+  myPublicFunction2 = function myFunction() { } // because fn is saved in a variables it is visible in the console.
+}
+
+export default Editor;
